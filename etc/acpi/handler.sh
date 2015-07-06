c96d8a4cfb43c95039736b5b9538184ebdb22ad9
Anna Ivanova <kalter@deadsoftware.ru>
2015-07-06 17:30:50 +0500

    update acpi

--- etc/acpi/handler.sh
+++ etc/acpi/handler.sh
@@ -1,77 +1,37 @@
 #!/bin/bash
-# Default acpi script that takes an entry for all actions
 
 case "$1" in
-    button/power)
-        case "$2" in
-            PBTN|PWRF)
-                logger 'PowerButton pressed'
-                ;;
-            *)
-                logger "ACPI action undefined: $2"
-                ;;
-        esac
-        ;;
-    button/sleep)
-        case "$2" in
-            SLPB|SBTN)
-                logger 'SleepButton pressed'
-                ;;
-            *)
-                logger "ACPI action undefined: $2"
-                ;;
-        esac
-        ;;
-    ac_adapter)
-        case "$2" in
-            AC|ACAD|ADP0)
-                case "$4" in
-                    00000000)
-                        logger 'AC unpluged'
-                        ;;
-                    00000001)
-                        logger 'AC pluged'
-                        ;;
-                esac
-                ;;
-            *)
-                logger "ACPI action undefined: $2"
-                ;;
-        esac
-        ;;
-    battery)
-        case "$2" in
-            BAT0)
-                case "$4" in
-                    00000000)
-                        logger 'Battery online'
-                        ;;
-                    00000001)
-                        logger 'Battery offline'
-                        ;;
-                esac
-                ;;
-            CPU0)
-                ;;
-            *)  logger "ACPI action undefined: $2" ;;
-        esac
-        ;;
-    button/lid)
-        case "$3" in
-            close)
-                logger 'LID closed'
-                ;;
-            open)
-                logger 'LID opened'
-                ;;
-            *)
-                logger "ACPI action undefined: $3"
-                ;;
-    esac
-    ;;
-    *)
-        logger "ACPI group/action undefined: $1 / $2"
-        ;;
-esac
+	button/lid)
+		case "$3" in
+			close)
+				logger 'LID closed'
+				sudo pm-suspend
+			;;
 
-# vim:set ts=4 sw=4 ft=sh et:
+			open)
+				logger 'LID opened'
+			;;
+
+			*)
+				logger "ACPI action undefined: $3"
+			;;
+		esac
+	;;
+
+	button/sleep)
+		case "$2" in
+			SBTN)
+				logger "ACPI button/sleep"
+				echo -n mem | sudo tee /sys/power/state > /dev/null
+			;;
+
+			*)
+				logger "ACPI action undefined: $2"
+			;;
+		esac
+	;;
+
+	*)
+		logger "ACPI group/action undefined: $1 / $2"
+	;;
+esac
