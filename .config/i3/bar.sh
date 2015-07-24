bb303311a13f51452433fd741669f59b8d24ff76
Anna Ivanova <kalter@deadsoftware.ru>
2015-07-24 23:23:35 +0500

    refactoring: i3bar

--- .config/i3/bar.sh
+++ .config/i3/bar.sh
@@ -15,41 +15,38 @@ color_std='#CCCCCC'
 color_waring='#CCCC00'
 color_danger='#CC0000'
 
-#	}}}
-
-
-#	{{{ Icons
-
-iconbyname() {
-	echo '"icon":"'$HOME/.config/i3/icons/$1'.xbm"'
-}
-
-icon_kernel=`iconbyname arch`
-icon_sound_on=`iconbyname sound_on`
-icon_sound_off=`iconbyname sound_off`
-icon_capslock=`iconbyname capslock`
-icon_numlock=`iconbyname numlock`
-icon_cpu=`iconbyname cpu`
-icon_language=`iconbyname kbd`
-icon_brightness=`iconbyname sun`
-icon_csq=`iconbyname csq`
-
-for icon_battary in 0 10 20 30 40 50 60 70 80 90 100; do
-	var="icon_battary_$icon_battary"
-	full_path=`iconbyname "battary/battary_$icon_battary"`
-	declare -g "$var=$full_path"
-done
-
 icon_color='"icon_color":"'$color_white'"'
 
 #	}}}
 
 
+iconbyname() {
+	echo '"icon":"'$HOME/.config/i3/icons/$1'.xbm"'
+}
+
+
+##
+## Kernel version
+##
+
 init_kernel() {
 	## JSON output
 	full_text='"full_text":"'`uname -sr`'"'
 	color='"color":"'$color_std'"'
+	icon_kernel=`iconbyname arch`
 	kernel='{'$full_text','$color','$icon_kernel','$icon_color'},'
+}
+
+#get_kernel() {
+#}
+
+
+##
+## Keyboard
+##
+
+init_language() {
+	icon_language=`iconbyname kbd`
 }
 
 get_language() {
@@ -64,12 +61,16 @@ get_language() {
 }
 
 
-#	{{{ Signal Strength
+##
+## Signal Strength
+##
 
 init_csq() {
 	# csqd, yes...
 	sudo killall csqd
 	sudo $HOME/bin/csqd "/dev/ttyUSB2" &
+
+	icon_csq=`iconbyname csq`
 }
 
 get_csq() {
@@ -81,8 +82,14 @@ get_csq() {
 	csq='{'$full_text','$color','$icon_csq','$icon_color'},'
 }
 
-#	}}}
 
+##
+## Num Lock state
+##
+
+init_numlock() {
+	icon_numlock=`iconbyname numlock`
+}
 
 get_numlock() {
 	numlock=`xset q\
@@ -94,6 +101,15 @@ get_numlock() {
 	full_text='"full_text":"'$numlock'"'
 	color='"color":"'$color_std'"'
 	numlock='{'$full_text','$color','$icon_numlock','$icon_color'},'
+}
+
+
+##
+## Caps Lock state
+##
+
+init_capslock() {
+	icon_capslock=`iconbyname capslock`
 }
 
 get_capslock() {
@@ -114,6 +130,15 @@ get_capslock() {
 	color1='"icon_color":"'$color1'"'
 
 	capslock='{'$full_text','$color0','$icon_capslock','$color1'},'
+}
+
+
+##
+## Heating CPU
+##
+
+init_heating_cpu() {
+	icon_cpu=`iconbyname cpu`
 }
 
 get_heating_cpu() {
@@ -148,6 +173,15 @@ get_heating_cpu() {
 	heating_cpu='{'$full_text','$color0','$icon_cpu','$color1'},'
 }
 
+
+##
+## Brightness level
+##
+
+init_brightness() {
+	icon_brightness=`iconbyname sun`
+}
+
 get_brightness() {
 	brightness=`cat /sys/class/backlight/intel_backlight/brightness`
 	max_brightness=`cat /sys/class/backlight/intel_backlight/max_brightness`
@@ -158,6 +192,16 @@ get_brightness() {
 	full_text='"full_text":"'$brightness_level'%"'
 	color='"color":"'$color_std'"'
 	brightness='{'$full_text','$color','$icon_brightness','$icon_color'},'
+}
+
+
+##
+## Sound state (level and mute)
+##
+
+init_sound() {
+	icon_sound_on=`iconbyname sound_on`
+	icon_sound_off=`iconbyname sound_off`
 }
 
 get_sound() {
@@ -181,41 +225,59 @@ get_sound() {
 	sound='{'$full_text','$color','$icon','$icon_color'},'
 }
 
-get_battary_level() {
-	battary_level=`cat /sys/class/power_supply/BAT0/capacity`
+
+##
+## Battery level
+##
+
+init_battery_level() {
+	for icon_battery in 0 10 20 30 40 50 60 70 80 90 100; do
+		var="icon_battery_$icon_battery"
+		full_path=`iconbyname "battery/battery_$icon_battery"`
+		declare -g "$var=$full_path"
+	done
+}
+
+get_battery_level() {
+	battery_level=`cat /sys/class/power_supply/BAT0/capacity`
 
 	## JSON output
-	full_text='"full_text":"'$battary_level'%"'
+	full_text='"full_text":"'$battery_level'%"'
 
-	battary_color0=$color_std   # text
-	battary_color1=$color_white # icon
-	if (( "$battary_level" <= "25" )); then
-		battary_color0=$color_waring
-		battary_color1=$color_waring
-		if (( "$battary_level" <= "10" )); then
-			battary_color0=$color_danger
-			battary_color1=$color_danger
+	battery_color0=$color_std   # text
+	battery_color1=$color_white # icon
+	if (( "$battery_level" <= "25" )); then
+		battery_color0=$color_waring
+		battery_color1=$color_waring
+		if (( "$battery_level" <= "10" )); then
+			battery_color0=$color_danger
+			battery_color1=$color_danger
 		fi
 	fi
-	color0='"color":"'$battary_color0'"'
-	color1='"icon_color":"'$battary_color1'"'
+	color0='"color":"'$battery_color0'"'
+	color1='"icon_color":"'$battery_color1'"'
 
-	case "$battary_level" in
-		[0-5])           icon_battary=$icon_battary_0   ;;
-		[6-9] | 1[0-5])  icon_battary=$icon_battary_10  ;;
-		1[6-9] | 2[0-5]) icon_battary=$icon_battary_20  ;;
-		2[6-9] | 3[0-5]) icon_battary=$icon_battary_30  ;;
-		3[6-9] | 4[0-5]) icon_battary=$icon_battary_40  ;;
-		4[6-9] | 5[0-5]) icon_battary=$icon_battary_50  ;;
-		5[6-9] | 6[0-5]) icon_battary=$icon_battary_60  ;;
-		6[6-9] | 7[0-5]) icon_battary=$icon_battary_70  ;;
-		7[6-9] | 8[0-5]) icon_battary=$icon_battary_80  ;;
-		8[6-9] | 9[0-5]) icon_battary=$icon_battary_90  ;;
-		9[6-9] | 100)    icon_battary=$icon_battary_100 ;;
+	case "$battery_level" in
+		[0-5])           icon_battery=$icon_battery_0   ;;
+		[6-9] | 1[0-5])  icon_battery=$icon_battery_10  ;;
+		1[6-9] | 2[0-5]) icon_battery=$icon_battery_20  ;;
+		2[6-9] | 3[0-5]) icon_battery=$icon_battery_30  ;;
+		3[6-9] | 4[0-5]) icon_battery=$icon_battery_40  ;;
+		4[6-9] | 5[0-5]) icon_battery=$icon_battery_50  ;;
+		5[6-9] | 6[0-5]) icon_battery=$icon_battery_60  ;;
+		6[6-9] | 7[0-5]) icon_battery=$icon_battery_70  ;;
+		7[6-9] | 8[0-5]) icon_battery=$icon_battery_80  ;;
+		8[6-9] | 9[0-5]) icon_battery=$icon_battery_90  ;;
+		9[6-9] | 100)    icon_battery=$icon_battery_100 ;;
 	esac
 
-	battary_level='{'$full_text','$color0','$icon_battary','$color1'},'
+	battery_level='{'$full_text','$color0','$icon_battery','$color1'},'
 }
+
+
+##
+## Date and time
+##
 
 get_date() {
 	date=`date +%a\ %d.%m.%Y\
@@ -227,9 +289,7 @@ get_date() {
 	date='{'$full_text','$color'},'
 }
 
-
-#	{{{ The last block
-
+## The last block
 get_time() {
 	time=`date +%H:%M:%S`
 
@@ -239,19 +299,17 @@ get_time() {
 	time='{'$full_text','$color'}'
 }
 
-#	}}}
-
 
 blocks=(
 	[10]=kernel
 	[20]=language
 	[30]=numlock
-#	[40]=capslock
-#	[50]=csq
+	[40]=capslock
+	[50]=csq
 	[60]=heating_cpu
-#	[70]=brightness
+	[70]=brightness
 	[80]=sound
-	[90]=battary_level
+	[90]=battery_level
 	[100]=date
 	[110]=time
 )
